47e762ef68139701e2bdb8fb516e0110a7bdac0c
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-08-06 11:06:22 -0700

    todo file main CRUD Done

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/nomankhokhar/todo-app
 
 go 1.22.5
+
+require (
+	github.com/alexeyco/simpletable v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+)
--- todo.go
+++ todo.go
@@ -1,10 +1,128 @@
 package todo
 
-import "time"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/alexeyco/simpletable"
+)
 
 type item struct {
 	Task        string
 	Done        bool
 	CreatedAt   time.Time
 	CompletedAt time.Time
+}
+
+type Todos []item
+
+func (t *Todos) Complete(index int) error {
+	ls := *t
+	if index <= 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+
+	ls[index-1].Done = true
+	ls[index-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+func (t *Todos) Delete(index int) error {
+	ls := *t
+	if index < 0 || index > len(ls) {
+		return errors.New("Invalid index")
+	}
+
+	*t = append(ls[:index], ls[index+1:]...)
+
+	return nil
+}
+
+func (t *Todos) Load(filename string) error {
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return nil
+		}
+		return err
+	}
+
+	if len(file) == 0 {
+		return err
+	}
+
+	err = json.Unmarshal(file, t)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Todos) Save(filename string) error {
+	data, err := json.Marshal(t)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(filename, data, 0644)
+}
+
+func (t *Todos) Print() {
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done"},
+			{Align: simpletable.AlignCenter, Text: "CreatedAt"},
+			{Align: simpletable.AlignCenter, Text: "CompletedAt"},
+		},
+	}
+
+	var cells [][]*simpletable.Cell
+
+	for i, item := range *t {
+		i++
+		task := blue(item.Task)
+		done := blue("no")
+		if item.Done {
+			task = green(fmt.Sprintf("\u2713 %s", item.Task))
+			done = green("yes")
+		}
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", i)},
+			{Text: task},
+			{Text: done},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+		})
+	}
+
+	table.Body = &simpletable.Body{Cells: cells}
+
+	table.Footer = &simpletable.Footer{Cells: []*simpletable.Cell{
+		{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("Developed by Noman Ali You have %d pending todos", t.CountPending()))},
+	}}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
+}
+
+func (t *Todos) CountPending() int {
+	total := 0
+
+	for _, item := range *t {
+		if !item.Done {
+			total++
+		}
+	}
+	return total
 }

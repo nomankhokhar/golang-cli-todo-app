48b3c78a69b5f52aeae321e5bc4ac019dc65b3d6
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-08-06 11:32:17 -0700

    all functionality workong

--- todo/cmd/main.go
+++ todo/cmd/main.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/nomankhokhar/todo-app"
+)
+
+const (
+	todoFile = ".todos.json"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	add := flag.Bool("add", false, "add a new todo")
+	complete := flag.Int("complete", 0, "mark a todo as completed")
+	del := flag.Int("del", 0, "delete a todo")
+	list := flag.Bool("list", false, "list all todos")
+
+	flag.Parse()
+
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		task, err := getInput(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+		todos.Add(task)
+		err = todos.Save(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *complete > 0:
+		err := todos.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Save(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+	case *del > 0:
+		err := todos.Delete(*del)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+		err = todos.Save(todoFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+			os.Exit(1)
+		}
+
+	case *list:
+		todos.Print()
+	default:
+		fmt.Println("Please specify an action")
+		os.Exit(0)
+	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", fmt.Errorf("task cannot be empty")
+	}
+
+	return text, nil
 }
